73f0a8b00a7671591e548ce4ded5f3a8b81936b2
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-20 20:46:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,13 +3,15 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        },
+        :name => "Lord Montague",
+        :age => "53"
+      },
       :matriarch => {
         #your key/value pairs here
-        },
+      },
       :hero => {
         #your key/value pairs here
-        },
+      },
       :hero_friends => []
    }, 
    :capulet => {
